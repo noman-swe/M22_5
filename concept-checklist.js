f8b5f46b6e7871dd13d0da80a9baf8553022ac1a
noman-swe <noman.swe130@gmail.com>
2022-02-05 02:34:54 +0600

    concept checklist

---
+++ concept-checklist.js
@@ -0,0 +1,74 @@
+// write 3 variables (number, string, boolean)
+const number = 1;
+const txt = 'i go to school';
+var schoolOff = true;
+//variables let & const
+let i = 0;
+while(i < 10){
+    //console.log(i);
+    i++
+} 
+
+const name = "Abdullah - Al Noman";
+
+//3 variable decleare korte hobe & tader sobar sathy sobar +, -, *, /, % korte hobe
+
+//addition
+function addition(num1, num2, num3){
+    const sum = num1 + num2 + num3;
+    return sum;
+}
+const sum = addition(20, 1, 9);
+console.log(sum);
+
+//substruction
+function substruction(num1, num2, num3){
+    const subs = (num1 - num2) - num3;
+    return subs;
+}
+const subs = substruction(121, 45, 90);
+console.log(subs);
+
+//multiply
+function multiply(num1, num2, num3){
+    const multi = (num1 * num2) * num3;
+    return multi;
+}
+const multi = multiply(121, 45, 90);
+console.log(multi);
+
+//division
+function division(num1, num2, num3){
+    const divi = (num1 / num2) / num3;
+    return divi;
+}
+const divi = division(121, 45, 90);
+console.log(divi);
+
+// %
+function vagShes(num1, num2, num3){
+    let multi = (num1 * num2) 
+    let vashess = multi % num3;
+    return vashess;
+}
+const vashess = vagShes(121, 45, 90);
+console.log(vashess);
+
+// 4 comparison
+
+//5 full fill 2 conditions with &&
+
+//6 if else statement 
+
+
+//7. while loop 7 to 19 odd digits finding
+
+// 8. array declearation: 4th element update or change, add element, check specific value exsists or not
+
+//9. use for loop to display everyelements
+
+//10 from a array display only the numbers bigger than 80
+
+//11. write a function that takes three number and return multipication of th three numbers
+
+//12.declare an object and change any property of that object
